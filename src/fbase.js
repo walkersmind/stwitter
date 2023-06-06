858d6adbf543336130834e4e35f6a062032390ee
Jeong <jeong@HwiJeongs-MacBook-Pro.local>
2023-06-06 16:48:41 +0800

    Login Form with Email, Google, Github

--- src/fbase.js
+++ src/fbase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
@@ -15,5 +11,7 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
 
+const app = initializeApp(firebaseConfig);
+const authService = getAuth(app);
 // Initialize Firebase
-export const firebase = initializeApp(firebaseConfig);
+export default authService;
