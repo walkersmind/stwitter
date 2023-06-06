990b629886396acff3a908e38e9d91ba4c32415b
walkersmind <95206425+walkersmind@users.noreply.github.com>
2023-06-06 16:54:03 +0800

    Login Form with Email, Google, Github (#2)
    Co-authored-by: Jeong <jeong@HwiJeongs-MacBook-Pro.local>

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import AppRouter from "./Router";
+import authService from "../fbase";
+import Auth from "../routes/Auth";
 
 function App() {
-  return <AppRouter />;
+  console.log(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <Auth />
+      <footer>&copy;{new Date().getFullYear()} Stwitter</footer>
+    </>
+  );
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          onChange={onChange}
+        ></input>
+        <input
+          name="password"
+          type="text"
+          placeholder="Password"
+          required
+          onChange={onChange}
+        ></input>
+        <input
+          type="button"
+          value="Continue with Email"
+          onSubmit={onSubmit}
+        ></input>
+        <input
+          type="button"
+          value="Continue with Google"
+          onSubmit={onSubmit}
+        ></input>
+        <input
+          type="button"
+          value="Continue with Github"
+          onSubmit={onSubmit}
+        ></input>
+      </form>
+    </div>
+  );
+};
 
 export default Auth;
